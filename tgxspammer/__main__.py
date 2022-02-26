f98c34d830feda5ae66e3d692a34e73ca4c666a9
SUKHPAL <86770159+SUKHPAL443@users.noreply.github.com>
2022-02-26 19:20:27 +0530

    Update __main__.py

--- tgxspammer/__main__.py
+++ tgxspammer/__main__.py
@@ -1,1 +1,54 @@
+import asyncio
+import sys
+from sys import argv
+import glob
+from pathlib import Path
+from tgxspammer.utils import load_plugins
+import logging
+from telethon import events
+from . import Riz, Riz2, Riz3, Riz4, Riz5, Riz6, Riz7, Riz8, Riz9, Riz10
 
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+
+path = "tgxspammer/plugins/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
+        load_plugins(plugin_name.replace(".py", ""))
+
+print("TGX SPAM BOT SUCCEDDFULLY DEPLOYED -!")
+print("Enjoy! Do visit @MAMBA_NETWORK")
+
+if __name__ == "__main__":
+    Riz.run_until_disconnected()
+    
+if __name__ == "__main__":
+    Riz2.run_until_disconnected()
+
+if __name__ == "__main__":
+    Riz3.run_until_disconnected()
+    
+if __name__ == "__main__":
+    Riz4.run_until_disconnected()
+
+if __name__ == "__main__":
+    Riz5.run_until_disconnected()
+    
+if __name__ == "__main__":
+    Riz6.run_until_disconnected()
+    
+if __name__ == "__main__":
+    Riz7.run_until_disconnected()
+
+if __name__ == "__main__":
+    Riz8.run_until_disconnected()
+    
+if __name__ == "__main__":
+    Riz9.run_until_disconnected()
+
+if __name__ == "__main__":
+    Riz10.run_until_disconnected()    
