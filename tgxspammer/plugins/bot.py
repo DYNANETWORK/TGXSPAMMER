a69564aa97a23424e22447cfc46a8d56e00cb996
SUKHPAL <86770159+SUKHPAL443@users.noreply.github.com>
2022-02-26 19:55:43 +0530

    Create bot.py

---
+++ tgxspammer/plugins/bot.py
@@ -0,0 +1,200 @@
+import os
+import asyncio
+import sys
+import git
+import heroku3
+from tgxspammer import Riz, Riz2, Riz3, Riz4, Riz5 , Riz6, Riz7, Riz8, Riz9, Riz10, OWNER_ID, SUDO_USERS, HEROKU_APP_NAME, HEROKU_API_KEY, rizoelversion
+from tgxspammer import CMD_HNDLR as hl
+from telethon.tl.functions.users import GetFullUserRequest
+from tgxspammer import ALIVE_PIC
+from telethon import events, version, Button
+from telethon.tl.custom import button
+from time import time
+from datetime import datetime
+
+MAM_PIC = ALIVE_PIC if ALIVE_PIC else "https://telegra.ph/file/2526a514b8ccd7b7cc380.jpg"
+  
+
+mamba = "✯ 𝓣𝓖𝓧 𝓢𝓟𝓐𝓜𝓑𝓞𝓣 ✯\n\n"
+mamba += f"═══════════════════\n"
+mamba += f"• **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ** : `3.9.6`\n"
+mamba += f"• **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ** : `{version.__version__}`\n"
+mamba += f"• **tgxsᴘᴀᴍ ᴠᴇʀsɪᴏɴ**  : `{tgxversion}`\n"
+mamba += f"═══════════════════\n\n"   
+
+                                  
+@Mam.on(events.NewMessage(incoming=True, pattern=r"\%salive(?: |$)(.*)" % hl))
+@Mam2.on(events.NewMessage(incoming=True, pattern=r"\%salive(?: |$)(.*)" % hl))
+@Mam3.on(events.NewMessage(incoming=True, pattern=r"\%salive(?: |$)(.*)" % hl))
+@Mam4.on(events.NewMessage(incoming=True, pattern=r"\%salive(?: |$)(.*)" % hl))
+@Mam5.on(events.NewMessage(incoming=True, pattern=r"\%salive(?: |$)(.*)" % hl))
+@Mam6.on(events.NewMessage(incoming=True, pattern=r"\%salive(?: |$)(.*)" % hl))
+@Mam7.on(events.NewMessage(incoming=True, pattern=r"\%salive(?: |$)(.*)" % hl))
+@Mam8.on(events.NewMessage(incoming=True, pattern=r"\%salive(?: |$)(.*)" % hl))
+@Mam9.on(events.NewMessage(incoming=True, pattern=r"\%salive(?: |$)(.*)" % hl))
+@Mam10.on(events.NewMessage(incoming=True, pattern=r"\%salive(?: |$)(.*)" % hl))
+async def alive(event):
+  if event.sender_id in SUDO_USERS:
+     await event.client.send_file(event.chat_id,
+                                  MAM_PIC,
+                                  caption=mamba,
+                                  buttons=[
+        [
+        Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/MAMBA_NETWORK"),
+        Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/MAMBA_X_SUPPORT")
+        ],
+        [
+        Button.url("• ʀᴇᴘᴏ •", "https://github.com/SUKHPAL443/TGXSPAMMER")
+        ]
+        ]
+        )
+    
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+
+@Mam.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@Mam2.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@Mam3.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@Mam4.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@Mam5.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@Mam6.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@Mam7.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@Mam8.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@Mam9.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@Mam10.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+async def ping(e):
+    if e.sender_id in SUDO_USERS:
+        start = datetime.now()
+        text = "Pong!"
+        event = await e.reply(text, parse_mode=None, link_preview=None )
+        end = datetime.now()
+        ms = (end-start).microseconds / 1000
+        await event.edit(f"▒█▀▀█ ▒█▀▀▀█ ▀▀█▀▀\n▒█▀▀▄ ▒█░░▒█ ░▒█░░\n▒█▄▄█ ▒█▄▄▄█ ░▒█░░\n\nϟ 𝓣𝓖𝓧 𝓢𝓟𝓐𝓜 ϟ︎ `{ms}` ᴍs")
+        
+        
+
+@Mam.on(events.NewMessage(incoming=True, pattern=r"\%srestart(?: |$)(.*)" % hl))
+@Mam2.on(events.NewMessage(incoming=True, pattern=r"\%srestart(?: |$)(.*)" % hl))
+@Mam3.on(events.NewMessage(incoming=True, pattern=r"\%srestart(?: |$)(.*)" % hl))
+@Mam4.on(events.NewMessage(incoming=True, pattern=r"\%srestart(?: |$)(.*)" % hl))
+@Mam5.on(events.NewMessage(incoming=True, pattern=r"\%srestart(?: |$)(.*)" % hl))
+@Mam6.on(events.NewMessage(incoming=True, pattern=r"\%srestart(?: |$)(.*)" % hl))
+@Mam7.on(events.NewMessage(incoming=True, pattern=r"\%srestart(?: |$)(.*)" % hl))
+@Mam8.on(events.NewMessage(incoming=True, pattern=r"\%srestart(?: |$)(.*)" % hl))
+@Mam9.on(events.NewMessage(incoming=True, pattern=r"\%srestart(?: |$)(.*)" % hl))
+@Mam10.on(events.NewMessage(incoming=True, pattern=r"\%srestart(?: |$)(.*)" % hl))
+async def restart(e):
+    if e.sender_id in SUDO_USERS:
+        text = "**Restarting Your TGX SPAMMER**.. Please Wait Until It Starts Again"
+        await e.reply(text, parse_mode=None, link_preview=None)
+        try:
+            await Riz.disconnect()
+        except Exception:
+            pass
+        try:
+            await Riz2.disconnect()
+        except Exception:
+            pass
+        try:
+            await Riz3.disconnect()
+        except Exception:
+            pass
+        try:
+            await Riz4.disconnect()
+        except Exception:
+            pass
+        try:
+            await Riz5.disconnect()
+        except Exception:
+            pass
+        try:
+            await Riz6.disconnect()
+        except Exception:
+            pass
+        try:
+            await Riz7.disconnect()
+        except Exception:
+            pass
+        try:
+            await Riz8.disconnect()
+        except Exception:
+            pass
+        try:
+            await Riz9.disconnect()
+        except Exception:
+            pass
+        try:
+            await Riz10.disconnect()
+        except Exception:
+            pass
+
+        os.execl(sys.executable, sys.executable, *sys.argv)
+        quit()
+        
+
+Heroku = heroku3.from_key(HEROKU_API_KEY)
+heroku_api = "https://api.heroku.com"
+sudousers = os.environ.get("SUDO_USER", None)
+
+@Mam.on(events.NewMessage(incoming=True, pattern=r"\%saddsudo(?: |$)(.*)" % hl))
+async def tb(event):
+    if event.sender_id == OWNER_ID:
+        ok = await event.reply("Adding user as a sudo...")
+        rizoel = "SUDO_USER"
+        if HEROKU_APP_NAME is not None:
+            app = Heroku.app(HEROKU_APP_NAME)
+        else:
+            await ok.edit("`[HEROKU]:" "\nPlease setup your` **HEROKU_APP_NAME**")
+            return
+        heroku_var = app.config()
+        if event is None:
+            return
+        try:
+            target = await get_user(event)
+        except Exception:
+            await ok.edit(f"Reply to a user.")
+        if sudousers:
+            newsudo = f"{sudousers} {target}"
+        else:
+            newsudo = f"{target}"
+        await ok.edit(f"**Added `{target}` ** as a sudo user 🔱 Restarting.. Please wait a minute...")
+        heroku_var[mamba] = newsudo   
+   
+     
+async def get_user(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        if previous_message.forward:
+            replied_user = await event.client(
+                GetFullUserRequest(previous_message.forward.sender_id)
+            )
+        else:
+            replied_user = await event.client(
+                GetFullUserRequest(previous_message.sender_id)
+            )
+    target = replied_user.user.id
+    return target
